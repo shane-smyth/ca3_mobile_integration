d4116815b23f29ad0b2e928b10731264aedcad3e
shane-smyth <158466904+shane-smyth@users.noreply.github.com>
2025-12-10 01:20:42 +0000

    feat(nav): update nav icons
    added material icons to navbar and Log.d

--- app/src/main/java/com/example/ca3/ui/navigation/Screen.kt
+++ app/src/main/java/com/example/ca3/ui/navigation/Screen.kt
@@ -1,8 +1,12 @@
 package com.example.ca3.ui.navigation
 
-sealed class Screen(val route: String, val label: String) {
-    object Home : Screen("home", "Home")
-    object Attendance : Screen("attendance", "Attendance")
-    object Classes : Screen("classes", "Classes")
-    object Profile : Screen("profile", "Profile")
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screen(val route: String, val label: String, val icon: ImageVector) {
+    object Home : Screen("home", "Home", Icons.Filled.Home)
+    object Attendance : Screen("attendance", "Attendance", Icons.Filled.EventAvailable)
+    object Classes : Screen("classes", "Classes", Icons.Filled.School)
+    object Profile : Screen("profile", "Profile", Icons.Filled.Person)
 }
